1fc62a090ce3800004e8ffc8aa7d671126631cd0
Kelly (Yutong) He <22101038+KellyYutongHe@users.noreply.github.com>
2023-12-05 12:44:07 -0500

    Update start_interactive.sh

--- start_interactive.sh
+++ start_interactive.sh
@@ -1,1 +1,1 @@
-srun --partition=atlas-interactive --gres=gpu:1 --cpus-per-task=8 --mem=32G --time=12:00:00 --pty bash
+srun --partition=atlas-interactive --gres=gpu:1 --cpus-per-task=8 --mem=8G --time=12:00:00 --pty bash
