2ca512674860317d9bc9ff18471a4b121c507d4a
Kelly (Yutong) He <22101038+KellyYutongHe@users.noreply.github.com>
2023-12-05 12:44:35 -0500

    Update noninteractive.sh

--- noninteractive.sh
+++ noninteractive.sh
@@ -11,6 +11,6 @@ echo "Log Folder:"${LOG_FOLDER}
 mkdir -p ${LOG_FOLDER}
 sbatch --output=${LOG_FOLDER}/%j.out --error=${LOG_FOLDER}/%j.err \
     --exclude=atlas1,atlas2,atlas3,atlas4,atlas5,atlas6 \
-    --nodes=1 --ntasks-per-node=1 --time=1-00:00:00 --mem=32G \
+    --nodes=1 --ntasks-per-node=1 --time=1-00:00:00 --mem=8G \
     --partition=atlas --cpus-per-task=2 \
     --gres=gpu:titanxp:${GPUS} --job-name=${JOBNAME} --wrap="${WRAP}"
